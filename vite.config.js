492b691e6c67486cccb2ab6ed8853f87102b4488
qinzhen <qz519189636@qq.com>
2023-07-29 15:01:04 +0800

    feat: update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pck from "./package.json"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+
+const genBaseUrl = (mode) => {
+  switch (mode) {
+    case "production":
+      return `/${pck.name}/`
+    default:
+      return "/"
+  }
+}
+
+export default defineConfig(({ mode }) => {
+  return {
+    base: genBaseUrl(mode),
+    build:{
+      outDir:"docs"
+    },
+    plugins: [react()],
+  }
 })
